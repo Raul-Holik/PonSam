eddcc2ce7f1cb97dc5f4b7bc7e7a98133b99e325
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-11-22 19:22:46 +0100

    Add files via upload

---
+++ PonSam/PonSam/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonSam
+{
+    class Brojevi
+    {
+        double broj;
+        public double Broj { get => broj; set => broj = value; }
+
+        public virtual double ApsVrijednost()
+        {
+            return 0;
+        }
+    }
+    class Cijeli:Brojevi
+    {
+        public override double ApsVrijednost()
+        {
+            return Broj;
+        }
+    }
+    class Decimalni : Brojevi
+    {
+        public override double ApsVrijednost()
+        {
+            Broj = 100.45;
+            return Broj;
+        }
+    }
+    class Pozitivni:Cijeli
+    {
+        public override double ApsVrijednost()
+        {
+            Broj = 100;
+            return Broj;
+        }
+    }
+    class Negativni : Cijeli
+    {
+        public override double ApsVrijednost()
+        {
+            Broj = -100;
+            return Broj;
+           
+        }
+        public override string ToString()
+        {
+            Broj = Convert.ToString();
+            return Broj;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozitivni x = new Pozitivni();
+            Negativni y = new Negativni();
+            Decimalni z = new Decimalni();
+
+            x.ApsVrijednost();
+            y.ApsVrijednost();
+            z.ApsVrijednost();
+
+            Console.WriteLine(x.ToString());
+            Console.WriteLine(y.ToString());
+            Console.WriteLine(z.ToString());
+            Console.ReadKey();
+        }
+    }
+}
